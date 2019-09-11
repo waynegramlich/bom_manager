f39a0ae266889b87e3e80fb893375b244de6ce62
Wayne C. Gramlich <Wayne@Gramlich.Net>
2019-09-11 14:34:26 -0700

    Transfered .net file reading code from bom_manager/bom.py to bom_kicad_plugin/kicad.py.

--- bom_manager/bom.py
+++ bom_manager/bom.py
@@ -8848,7 +8848,7 @@ class PriceBreak:
 class Project:
 
     # Project.__init__():
-    def __init__(self, name, revision, net_file_name, count, order,
+    def __init__(self, name, revision, cad_file_name, count, order,
                  positions_file_name=None, tracing=None):
         """ Initialize a new *Project* object (i.e. *self*) containing *name*, *revision*,
             *net_file_name*, *count*, *order*, and optionally *positions_file_name.
@@ -8857,7 +8857,7 @@ class Project:
         # Verify argument types:
         assert isinstance(name, str)
         assert isinstance(revision, str)
-        assert isinstance(net_file_name, str)
+        assert isinstance(cad_file_name, str)
         assert isinstance(count, int)
         assert isinstance(order, Order)
         assert isinstance(positions_file_name, str) or positions_file_name is None
@@ -8867,13 +8867,13 @@ class Project:
         next_tracing = None if tracing is None else tracing + " "
         if tracing is None:
             print(f"{tracing}=>Project.__init__(*, '{name}', '{revision}', '{net_file_name}', "
-                  f"*, '{positions_file_name}'")
+                  f"{count}, *, '{positions_file_name}'")
 
         # Load up *project* (i.e. *self*):
         project = self
         project.name = name
         project.revision = revision
-        project.net_file_name = net_file_name
+        project.cad_file_name = cad_file_name
         project.count = count
         project.positions_file_name = positions_file_name
         project.order = order
@@ -8884,8 +8884,15 @@ class Project:
         project.installed_pose_parts = []    # List[PosePart] project parts to be installed
         project.uninstalled_pose_parts = []  # List[PosePart] project parts not to be installed
 
-        # Read in the `.net` file named *net_file_name*:
-        project.net_file_read(tracing=next_tracing)
+        # Read in the *cad_file_name*:
+        success = False
+        cads = order.cads
+        for cad in cads:
+            assert isinstance(cad, Cad)
+            success = cad.file_read(cad_file_name, project, tracing=next_tracing)
+            if success:
+                break
+        assert success, f"Could not successfully read and process file '{cad_file_name}'!"
 
         # Wrap-up any requested *tracing*:
         if tracing is None:
@@ -9063,282 +9070,6 @@ class Project:
         # Wrap up any requested *tracing*:
         if tracing is not None:
             print(f"{tracing}<=Project_check('{name}')")
-
-    # Project.new_file_read():
-    def net_file_read(self, tracing=None):
-        """ Read in net file for the *Project* object (i.e. *self*).
-        """
-        # Verify argument types:
-        assert isinstance(tracing, str) or tracing is None
-
-        # Perform any requested *tracing*:
-        next_tracing = None if tracing is None else tracing + " "
-        if tracing is not None:
-            print(f"{tracing}=>Project.new_file_read()")
-
-        # Prevent accidental double of *project* (i.e. *self*):
-        project = self
-        pose_parts = project.all_pose_parts
-        assert len(pose_parts) == 0
-
-        # Process *net_file_name* adding footprints as needed:
-        errors = 0
-        net_file_name = project.net_file_name
-        # print("Read '{0}'".format(net_file_name))
-        if net_file_name.endswith(".net"):
-            with open(net_file_name, "r") as net_stream:
-                # Read contents of *net_file_name* in as a string *net_text*:
-                net_text = net_stream.read()
-                print(f"Read in file '{net_file_name}'")
-
-            # Parse *net_text* into *net_se* (i.e. net S-expression):
-            net_se = sexpdata.loads(net_text)
-            # print("\nsexpedata.dumps=", sexpdata.dumps(net_se))
-            # print("")
-            # print("net_se=", net_se)
-            # print("")
-
-            # Visit each *component_se* in *net_se*:
-            net_file_changed = False
-            #database = project.order.database
-            components_se = se_find(net_se, "export", "components")
-
-            # Each component has the following form:
-            #
-            #        (comp
-            #          (ref SW123)
-            #          (footprint nickname:NAME)              # May not be present
-            #          (libsource ....)
-            #          (sheetpath ....)
-            #          (tstamp xxxxxxxx))
-            # print("components=", components_se)
-            for component_index, component_se in enumerate(components_se[1:]):
-                # print("component_se=", component_se)
-                # print("")
-
-                # Grab the *reference* from *component_se*:
-                reference_se = se_find(component_se, "comp", "ref")
-                reference = reference_se[1].value()
-                # print("reference_se=", reference_se)
-                # print("")
-
-                # Find *part_name_se* from *component_se*:
-                part_name_se = se_find(component_se, "comp", "value")
-
-                # Suprisingly tedious, extract *part_name* as a string:
-                if isinstance(part_name_se[1], Symbol):
-                    part_name = part_name_se[1].value()
-                elif isinstance(part_name_se[1], int):
-                    part_name = str(part_name_se[1])
-                elif isinstance(part_name_se[1], float):
-                    part_name = str(part_name_se[1])
-                elif isinstance(part_name_se[1], str):
-                    part_name = part_name_se[1]
-                else:
-                    assert False, "strange part_name: {0}". \
-                      format(part_name_se[1])
-
-                # Strip *comment* out of *part_name* if it exists:
-                comment = ""
-                colon_index = part_name.find(':')
-                if colon_index >= 0:
-                    comment = part_name[colon_index + 1:]
-                    part_name = part_name[0:colon_index]
-
-                # Now see if we have a match for *part_name* in *database*:
-                project_part = project.project_part_find(part_name)
-
-                # We have a match; create the *pose_part*:
-                pose_part = PosePart(project, project_part, reference, comment)
-                project.pose_part_append(pose_part)
-
-                # Ignore footprints for now:
-                if False:
-                    # Grab *kicad_footprint* from *project_part*:
-                    kicad_footprint = project_part.kicad_footprint
-                    assert isinstance(kicad_footprint, str)
-
-                    # Grab *footprint_se* from *component_se* (if it exists):
-                    footprint_se = se_find(component_se, "comp", "footprint")
-                    # print("footprint_se=", footprint_se)
-                    # print("Part[{0}]:'{1}' '{2}' changed={3}".format(
-                    #    component_index, part_name, kicad_footprint, net_file_changed))
-
-                    # Either add or update the footprint:
-                    if footprint_se is None:
-                        # No footprint in the .net file; just add one:
-                        component_se.append(
-                          [Symbol("footprint"), Symbol("common:" + kicad_footprint)])
-                        print("Part {0}: Adding binding to footprint '{1}'".
-                              format(part_name, kicad_footprint))
-                        net_file_changed = True
-                    else:
-                        # We have a footprint in .net file:
-                        previous_footprint = footprint_se[1].value()
-                        previous_split = previous_footprint.split(':')
-                        current_split = kicad_footprint.split(':')
-                        assert len(previous_split) > 0
-                        assert len(current_split) > 0
-                        if len(current_split) == 2:
-                            # *kicad_footprint* has an explicit library,
-                            # so we can just use it and ignore
-                            # *previous_footprint*:
-                            new_footprint = kicad_footprint
-                        elif len(current_split) == 1 and len(previous_split) == 2:
-                            # *kicad_footprint* does not specify a library,
-                            # but the *previous_footprint* does.  We build
-                            # *new_foot_print* using the *previous_footprint*
-                            # library and the rest from *kicad_footprint*:
-                            new_footprint = \
-                              previous_split[0] + ":" + kicad_footprint
-                            # print("new_footprint='{0}'".format(new_footprint))
-                        elif len(current_split) == 1:
-                            new_footprint = "common:" + kicad_footprint
-                        else:
-                            assert False, ("previous_slit={0} current_split={1}".
-                                           format(previous_split, current_split))
-    
-                        # Only do something if it changed:
-                        if previous_footprint != new_footprint:
-                            # Since they changed, update in place:
-                            # if isinstance(project_part, AliasPart):
-                            #        print("**AliasPart.footprint={0}".
-                            #          format(project_part.kicad_footprint))
-                            print("Part '{0}': Footprint changed from '{1}' to '{2}'".
-                                  format(part_name, previous_footprint, new_footprint))
-                            footprint_se[1] = Symbol(new_footprint)
-                            net_file_changed = True
-
-            # Write out updated *net_file_name* if *net_file_changed*:
-            if net_file_changed:
-                print("Updating '{0}' with new footprints".
-                      format(net_file_name))
-                net_file = open(net_file_name, "wa")
-                # sexpdata.dump(net_se, net_file)
-                net_se_string = sexpdata.dumps(net_se)
-                # sexpdata.dump(net_se, net_file)
-
-                # Now use some regular expressions to improve formatting to be more like
-                # what KiCad outputs:
-                net_se_string = re.sub(" \\(design ", "\n  (design ", net_se_string)
-
-                # Sheet part of file:
-                net_se_string = re.sub(" \\(sheet ",       "\n    (sheet ",         net_se_string)
-                net_se_string = re.sub(" \\(title_block ", "\n      (title_block ", net_se_string)
-                net_se_string = re.sub(" \\(title ",       "\n        (title ",     net_se_string)
-                net_se_string = re.sub(" \\(company ",     "\n        (company ",   net_se_string)
-                net_se_string = re.sub(" \\(rev ",         "\n        (rev ",       net_se_string)
-                net_se_string = re.sub(" \\(date ",        "\n        (date ",      net_se_string)
-                net_se_string = re.sub(" \\(source ",      "\n        (source ",    net_se_string)
-                net_se_string = re.sub(" \\(comment ",     "\n        (comment ",   net_se_string)
-
-                # Components part of file:
-                net_se_string = re.sub(" \\(components ", "\n  (components ",    net_se_string)
-                net_se_string = re.sub(" \\(comp ",       "\n    (comp ",        net_se_string)
-                net_se_string = re.sub(" \\(value ",      "\n      (value ",     net_se_string)
-                net_se_string = re.sub(" \\(footprint ",  "\n      (footprint ", net_se_string)
-                net_se_string = re.sub(" \\(libsource ",  "\n      (libsource ", net_se_string)
-                net_se_string = re.sub(" \\(sheetpath ",  "\n      (sheetpath ", net_se_string)
-                net_se_string = re.sub(" \\(path ",       "\n      (path ",      net_se_string)
-                net_se_string = re.sub(" \\(tstamp ",     "\n      (tstamp ",    net_se_string)
-
-                # Library parts part of file
-                net_se_string = re.sub(" \\(libparts ",    "\n  (libparts ",    net_se_string)
-                net_se_string = re.sub(" \\(libpart ",     "\n    (libpart ",   net_se_string)
-                net_se_string = re.sub(" \\(description ", "\n      (description ",  net_se_string)
-                net_se_string = re.sub(" \\(fields ",      "\n      (fields ",  net_se_string)
-                net_se_string = re.sub(" \\(field ",       "\n        (field ", net_se_string)
-                net_se_string = re.sub(" \\(pins ",        "\n      (pins ",    net_se_string)
-                # net_se_string = re.sub(" \\(pin ",         "\n        (pin ",   net_se_string)
-
-                # Network portion of file:
-                net_se_string = re.sub(" \\(nets ", "\n  (nets ", net_se_string)
-                net_se_string = re.sub(" \\(net ",  "\n    (net ", net_se_string)
-                net_se_string = re.sub(" \\(node ", "\n      (node ", net_se_string)
-
-                # General substitutions:
-                # net_se_string = re.sub(" \\;", ";", net_se_string)
-                # net_se_string = re.sub(" \\.", ".", net_se_string)
-
-                net_file.write(net_se_string)
-                net_file.close()
-            # else:
-            #        print("File '{0}' not changed".format(net_file_name))
-
-        elif net_file_name.ends_with(".cmp"):
-            # Read in {cmp_file_name}:
-            cmp_file_name = net_file_name
-            cmp_stream = open(cmp_file_name, "r")
-            cmp_lines = cmp_stream.readlines()
-            cmp_stream.close()
-
-            # Process each {line} in {cmp_lines}:
-            database = project.database
-            errors = 0
-            line_number = 0
-            for line in cmp_lines:
-                # Keep track of {line} number for error messages:
-                line_number = line_number + 1
-
-                # There are three values we care about:
-                if line.startswith("BeginCmp"):
-                    # Clear out the values:
-                    reference = None
-                    part_name = None
-                    footprint = None
-                elif line.startswith("Reference = "):
-                    reference = line[12:-2]
-                elif line.startswith("ValeurCmp = "):
-                    part_name = line[12:-2]
-                    # print("part_name:{0}".format(part_name))
-                    double_underscore_index = part_name.find("__")
-                    if double_underscore_index >= 0:
-                        shortened_part_name = \
-                          part_name[:double_underscore_index]
-                        # print("Shorten part name '{0}' => '{1}'".
-                        #  format(part_name, shortened_part_name))
-                        part_name = shortened_part_name
-                elif line.startswith("IdModule  "):
-                    footprint = line[12:-2].split(':')[1]
-                    # print("footprint='{0}'".format(footprint))
-                elif line.startswith("EndCmp"):
-                    part = database.part_lookup(part_name)
-                    if part is None:
-                        # {part_name} not in {database}; output error message:
-                        print("File '{0}', line {1}: Part Name {2} ({3} {4}) not in database".
-                              format(cmp_file_name, line_number, part_name, reference, footprint))
-                        errors = errors + 1
-                    else:
-                        footprint_pattern = part.footprint_pattern
-                        if fnmatch.fnmatch(footprint, footprint_pattern):
-                            # The footprints match:
-                            pose_part = \
-                              PosePart(project, part, reference, footprint)
-                            project.pose_parts_append(pose_part)
-                            part.pose_parts.append(pose_part)
-                        else:
-                            print(("File '{0}',  line {1}: {2}:{3} Footprint" +
-                                   "'{4}' does not match database '{5}'").format(
-                                   cmp_file_name, line_number,
-                                   reference, part_name, footprint,
-                                   footprint_pattern))
-                            errors = errors + 1
-                elif (line == "\n" or line.startswith("TimeStamp") or
-                      line.startswith("EndListe") or line.startswith("Cmp-Mod V01")):
-                    # Ignore these lines:
-                    line = line
-                else:
-                    # Unrecognized {line}:
-                    print("'{0}', line {1}: Unrecognized line '{2}'".
-                          format(cmp_file_name, line_number, line))
-                    errors = errors + 1
-        else:
-            print("Net file '{0}' name does not have a recognized suffix".format(net_file_name))
-
-        # Wrap up any requested *tracing* and return *errors*:
-        if tracing is not None:
-            print(f"{tracing}<=Project.new_file_read()=>[...]")
-        return errors
 
     # Project.project_part_append():
     def pose_part_append(self, pose_part):
@@ -9578,8 +9309,8 @@ class ChoicePart(ProjectPart):
         for project_part in project_parts:
             projects = project_part.projects
             for project in projects:
-                net_file_name = project.net_file_name
-                projects_table[net_file_name] = project
+                cad_file_name = project.cad_file_name
+                projects_table[cad_file_name] = project
         union_projects = list(projects_table.values())
 
         # Load up *choice_part* (i.e. *self*):
